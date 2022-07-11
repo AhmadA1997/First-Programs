f0082c83f08f5a398758e9164d5605c456338b91
AhmadA1997 <109098895+AhmadA1997@users.noreply.github.com>
2022-07-11 12:04:38 -0500

    Create Program 1- Volume calculator.py

---
+++ Program 1- Volume calculator.py
@@ -0,0 +1,9 @@
+Print('Welcome! Let's calculate the Volume of your container!')
+
+X= int(input('What is the Length:')
+
+Y= int(input('What is the Width:')
+
+Z= int(input('What is the Height:')
+
+Print('The Volume of your container is:', x*y*z)
